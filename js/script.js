176a88ab974849e9f990d0c09be4c31da36a3dba
Marta Bruczko <marta.bruczko@gmail.com>
2020-11-10 19:46:29 +0100

    Add playerMove papier

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+
+let playerMove = `papier`;
+
+printMessage('Zagrałeś ' + 'papier' + '! Jeśli Mój ruch to kamień, to wygrywasz!');
