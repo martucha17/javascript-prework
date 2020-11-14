776abd5cd005286db5ece20a25f169b04aee4c3c
Marta Bruczko <marta.bruczko@gmail.com>
2020-11-14 13:42:38 +0100

    Change functions

--- js/script.js
+++ js/script.js
@@ -1,64 +1,62 @@
 {
     //HTML REFERENCES
 
-const rockBtn = document.querySelector('#rock-btn');
-const paperBtn = document.querySelector('#paper-btn');
-const scissorsBtn = document.querySelector('#scissors-btn');
+    const rockBtn = document.querySelector('#rock-btn');
+    const paperBtn = document.querySelector('#paper-btn');
+    const scissorsBtn = document.querySelector('#scissors-btn');
 
-// FUNCTIONS
+    // FUNCTIONS
 
-function getMoveName(argMoveId) {
-    if (argMoveId == 1) return 'kamień';
-    else if (argMoveId == 2) return 'papier';
-    else if (argMoveId == 3) return 'nożyce';
-}
+    const getMoveName = function (argMoveId) {
+        if (argMoveId == 1) return 'kamień';
+        else if (argMoveId == 2) return 'papier';
+        else if (argMoveId == 3) return 'nożyce';
+    }
 
-function displayResult(argComputerMove, argPlayerMove) {
-    let condition = (
-        (argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
-        (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
-        (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'))
+    const displayResult = function (argComputerMove, argPlayerMove) {
+        condition = (
+            (argComputerMove == 'kamień' && argPlayerMove == 'papier') ||
+            (argComputerMove == 'papier' && argPlayerMove == 'nożyce') ||
+            (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'))
 
-    if (condition) printMessage('Ty wygrywasz!');
-    else if (argComputerMove == argPlayerMove) printMessage('Remis!');
-    else printMessage('Ty przegrywasz!');
-}
+        if (condition) printMessage('Ty wygrywasz!');
+        else if (argComputerMove == argPlayerMove) printMessage('Remis!');
+        else printMessage('Ty przegrywasz!');
+    }
 
-function playGame(playerInput){
+    const playGame = function (playerInput) {
 
-    // clear messages
-    clearMessages();
+        // clear messages
+        clearMessages();
 
-    //compute moves
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
-    
-    // player moves
-    let playerMove = getMoveName(playerInput);
-    
-    // render player and computer chocies in HTML
-    printMessage('Twój ruch to: ' + playerMove);
-    printMessage('Mój ruch to: ' + computerMove);
+        //compute moves
+        randomNumber = Math.floor(Math.random() * 3 + 1);
+        computerMove = getMoveName(randomNumber);
 
-    //result
-    displayResult(computerMove, playerMove);
+        // player moves
+        playerMove = getMoveName(playerInput);
 
-    
-}
+        // render player and computer chocies in HTML
+        printMessage('Twój ruch to: ' + playerMove);
+        printMessage('Mój ruch to: ' + computerMove);
 
-//MAIN PROCESS
+        //result
+        displayResult(computerMove, playerMove);
+    }
 
-rockBtn.addEventListener('click', function() {
-    playGame(1);
-});
+    //MAIN PROCESS
 
-paperBtn.addEventListener('click', function() {
-    playGame(2);
-});
+    rockBtn.addEventListener('click', function () {
+        playGame(1);
+    });
 
-scissorsBtn.addEventListener('click', function() {
-    playGame(3);
-});
+    paperBtn.addEventListener('click', function () {
+        playGame(2);
+    });
+
+    scissorsBtn.addEventListener('click', function () {
+        playGame(3);
+    });
 }
 
 
